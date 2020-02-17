879408f6bb430b6f48b07d1465289b2f5f74ff76
Kathryn Armitage <kat_armitage1@hotmail.com>
2020-02-17 14:25:51 +0000

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:answer]
+    if @question != ''
+      if @question.upcase == @question
+        @answer = 'I can feel your motivation!'
+      elsif @question == 'I am going to work right now!'
+        # option
+      elsif @question.end_with?('?')
+        @answer = 'Silly question, get dressed and go to work!'
+      else
+        @answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
+end
+
+
